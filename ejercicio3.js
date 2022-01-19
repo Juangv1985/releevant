2a2d6045f2d5aeccf932121d363d6364144daa21
Juan <juangv1985@gmail.com>
2022-01-19 20:04:27 +0100

    modifico el ejercicio3.js

--- ejercicio3.js
+++ ejercicio3.js
@@ -2,20 +2,19 @@
 //Algoritmo que lea dos numeros y nos diga cual de ellos es mayor
 //o si son iguales(recuerda usar la estructura condicional if)
 
-
-
 const prompt = require("prompt-sync")();
 
 const number1 = Number(prompt("Introduce el primer numero: "));
 const number2 = Number(prompt("Introduce el segundo numero: "));
 
-
 if (number1 === number2) {
-    console.log("Los numeros son iguales ");
-} else if (number1 < number2){
-    console.log("el primer numero es menor");
+  console.log("Los numeros son iguales ");
+} else if (number1 < number2) {
+  console.log("el primer numero es menor");
 } else {
-    console.log("El segundo numero es menor");
+  console.log("El segundo numero es menor");
 }
 
 //Ahora con tres numeros diferentes.
+
+//COMENTARIO DE JUAN
